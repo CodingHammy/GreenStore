5382504eae20919a9bc4a3eac702d452e7d252c6
CodingHammy <Will.hamilton.job@gmail.com>
2023-10-14 11:14:37 +0100

    set up styles for contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,8 +3,12 @@ import classes from "./page.module.css";
 
 const Contacts = () => {
   return (
-    <main className={classes.main}>
-      <div className={classes.description}></div>
+    <main className={classes.something}>
+      <section className={classes.text_container}>
+        <div className={classes.text_left}></div>
+        <div className={classes.text_right}></div>
+        <div className={classes.mapBox}></div>
+      </section>
     </main>
   );
 };
